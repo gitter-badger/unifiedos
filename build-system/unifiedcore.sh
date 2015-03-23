0451b5da0452d78f56ac2b3945ef494f6a8e1d62
John Unland <opensourcejohn2112@gmail.com>
2015-03-23 12:44:44 -0500

    Added build-system for creation of the os

---
+++ build-system/unifiedcore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## This script is going to build the base system without any heavy packages.
+## Be aware that you must have the "live-build" package with all it's dependencies on your workstation.
+
+## Greeting
+
+## Make sure live-build is installed.
+
+## Make working dir.
+
+
+
